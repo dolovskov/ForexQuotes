304f317a734ab4840393ac6c626c56a54f32ec60
dolovskovvladislav <vladislav.dolovskov@osinit.com>
2019-02-09 11:01:08 +0300

    Initial Commit

---
+++ ForexQuotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForexQuotes
+//
+//  Created by Доловсков Владислав on 09/02/2019.
+//  Copyright © 2019 Доловсков Владислав. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
